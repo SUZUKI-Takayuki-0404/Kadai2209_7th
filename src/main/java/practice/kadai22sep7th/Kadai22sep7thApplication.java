f81f7d5f0fd04bd1fc8d25b8453003cbd6d4584b
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-11-21 21:49:47 +0900

    Rename Kadai22sep7thApplication-class

--- src/main/java/practice/kadai22sep7th/Kadai22sep7thApplication.java
+++ src/main/java/practice/kadai22sep7th/Kadai22sep7thApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Kadai22097thApplication {
+public class Kadai22sep7thApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Kadai22097thApplication.class, args);
+        SpringApplication.run(Kadai22sep7thApplication.class, args);
     }
 
 }
