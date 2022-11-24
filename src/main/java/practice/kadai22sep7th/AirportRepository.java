54c891fec9d8d5700423809dd065e20ec778e7d7
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-11-24 20:02:52 +0900

    Update Servicr/Repository-class

--- src/main/java/practice/kadai22sep7th/AirportRepository.java
+++ src/main/java/practice/kadai22sep7th/AirportRepository.java
@@ -15,55 +15,36 @@ public class AirportRepository {
     private JdbcTemplate jdbcTemplate;
 
     public Optional<Map<String, Object>> findByCode(String airportCode) {
-
         String query = "SELECT * FROM airports WHERE airportCode=?";
-
         Map<String, Object> foundAirport;
-
         try {
-
             foundAirport = jdbcTemplate.queryForMap(query, airportCode);
-
         } catch (org.springframework.dao.EmptyResultDataAccessException e) {
-
             foundAirport = null;
-
         }
-
         return Optional.ofNullable(foundAirport);
     }
 
     public List<String> findAllCode() {
-
         String sql = "SELECT airportCode FROM airports";
-
         List<String> foundCodeList = new ArrayList<>();
-
         SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql);
-
         while (rowSet.next()) {
             foundCodeList.add(rowSet.getString("airportCode"));
         }
-
         return foundCodeList;
-
     }
 
     public List<AirportEntity> findAllData() {
-
         String sql = "SELECT * FROM airports";
-
         List<AirportEntity> allDataList = new ArrayList<>();
-
         SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql);
-
         while (rowSet.next()) {
             allDataList.add(new AirportEntity(
                     rowSet.getString("airportCode"),
                     rowSet.getString("airportName"),
                     rowSet.getString("country")));
         }
-
         return allDataList;
     }
 
--- src/main/java/practice/kadai22sep7th/AirportService.java
+++ src/main/java/practice/kadai22sep7th/AirportService.java
@@ -23,37 +23,27 @@ public class AirportService {
         String country;
 
         if (repository.findByCode(airport_code).isPresent()) {
-
             Map<String, Object> map = repository.findByCode(airport_code).get();
             airportCode = (String) map.get("airportCode");
             airportName = (String) map.get("airportName");
             country = (String) map.get("country");
-
             return new AirportEntity(airportCode, airportName, country);
-
         } else {
-
             throw new AirportNotFoundException(airport_code + " Not Found");
-
         }
     }
 
     public List<String> getAllAirportCodeList() {
-
         return repository.findAllCode();
     }
 
     public Map<String, List<String>> getAllDataMap() {
-
-        Map<String, List<String>> allDataMap = new HashMap<>();
-
         List<AirportEntity> allDataList = repository.findAllData();
-
+        Map<String, List<String>> allDataMap = new HashMap<>();
         allDataList.forEach(airportEntity -> {
             allDataMap.put(airportEntity.getAirportCode(),
                     Arrays.asList(airportEntity.getAirportName(), airportEntity.getCountry()));
         });
-
         return allDataMap;
     }
 
