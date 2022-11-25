1a4c00178457c8ea79701f2fb1471b3313962451
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-11-25 23:47:01 +0900

    Update airpotCodeResponse-class (update constructor)

--- src/main/java/practice/kadai22sep7th/AirportCodeResponse.java
+++ src/main/java/practice/kadai22sep7th/AirportCodeResponse.java
@@ -2,14 +2,22 @@ package practice.kadai22sep7th;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-@AllArgsConstructor
-@Data
+@Getter
+@Setter
 public class AirportCodeResponse {
 
     private String message;
     private List<String> airportCodes;
 
+    public AirportCodeResponse(String message, List<AirportEntity> allAirports) {
+        this.message = message;
+        this.airportCodes = allAirports.stream()
+                .map(airportEntity -> airportEntity.getAirportCode())
+                .toList();
+    }
 }
