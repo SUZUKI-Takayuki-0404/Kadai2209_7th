c4c811baf5e3e21f148383dafe4f884ec0ebc6bf
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-11-25 23:04:27 +0900

    Clean Service-class (delete unneeded method)

--- src/main/java/practice/kadai22sep7th/AirportService.java
+++ src/main/java/practice/kadai22sep7th/AirportService.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Service;
 import practice.kadai22sep7th.exceptionhandelers.AirportNotFoundException;
 import practice.kadai22sep7th.mapper.AirportMapper;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @AllArgsConstructor
 @Service
@@ -17,22 +14,12 @@ public class AirportService {
 
     private AirportMapper airportMapper;
 
-    public AirportEntity getAirportEntity(String airportCode) {
+    public AirportEntity getAirport(String airportCode) {
         return airportMapper.findById(airportCode).orElseThrow(() -> new AirportNotFoundException(airportCode + " Not Found"));
     }
 
-    public List<String> getAllAirportCodeList() {
-        return airportMapper.findAllCode();
-    }
-
-    public Map<String, List<String>> getAllDataMap() {
-        List<AirportEntity> allDataList = airportMapper.findAll();
-        Map<String, List<String>> allDataMap = new HashMap<>();
-        allDataList.forEach(airportEntity -> {
-            allDataMap.put(airportEntity.getAirportCode(),
-                    Arrays.asList(airportEntity.getAirportName(), airportEntity.getCountry()));
-        });
-        return allDataMap;
+    public List<AirportEntity> getAllAirports() {
+        return airportMapper.findAll();
     }
 
 }
