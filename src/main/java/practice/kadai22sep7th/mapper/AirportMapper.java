a4f7d29ac08bd8ceec01e8a6a4b24866bf84ce73
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-11-25 20:47:49 +0900

    Update Mapper-class (delete unneeded-method)

--- src/main/java/practice/kadai22sep7th/mapper/AirportMapper.java
+++ src/main/java/practice/kadai22sep7th/mapper/AirportMapper.java
@@ -1,7 +1,6 @@
 package practice.kadai22sep7th.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import practice.kadai22sep7th.AirportEntity;
 
 import java.util.List;
@@ -16,7 +15,12 @@ public interface AirportMapper {
     @Select("SELECT * FROM airports WHERE airportCode = #{airportCode}")
     Optional<AirportEntity> findById(String airportCode);
 
-    @Select("SELECT airportCode FROM airports")
-    List<String> findAllCode();
+    @Insert("INSERT INTO airports (airportCode, airportName, Country) VALUES (#{airportCode}, #{airportName}, #{Country})")
+    void create(String airportCode, String airportName, String Country);
 
+    @Update("UPDATE airports SET airportName = #{airportName}, Country = #{Country} WHERE airportCode = #{airportCode}")
+    boolean update(String airportCode, String airportName, String Country);
+
+    @Delete("DELETE FROM airports WHERE airportCode = #{airportCode}")
+    boolean deleteById(String airportCode);
 }
