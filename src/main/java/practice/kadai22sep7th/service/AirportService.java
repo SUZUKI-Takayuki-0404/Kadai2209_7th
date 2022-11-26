cc631ca3ac9d659cd9cf9266ec0dc99ac27ad6f0
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-11-26 21:17:27 +0900

    Update AirportService-class

--- src/main/java/practice/kadai22sep7th/service/AirportService.java
+++ src/main/java/practice/kadai22sep7th/service/AirportService.java
@@ -1,6 +1,7 @@
 package practice.kadai22sep7th.service;
 
 import practice.kadai22sep7th.entity.AirportEntity;
+import practice.kadai22sep7th.exceptionhandelers.AirportCreationFailedException;
 
 import java.util.List;
 
@@ -9,5 +10,7 @@ public interface AirportService {
     public AirportEntity getAirport(String airportCode);
 
     public List<AirportEntity> getAllAirports();
+
+    public AirportEntity createAirport(String airportCode, String airportName, String country) throws AirportCreationFailedException;
 
 }
